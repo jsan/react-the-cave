25a1c9e47798bde9072ace1b3884944c7f7d20b2
Jorge Sansone <jorge.sansone@gmail.com>
2023-07-10 16:51:56 +0100

    Added 3 cards to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 import img001 from './images/image01.png'
+import img003 from './images/image03.png'
 // import Button from 'react-bootstrap/Button'
 import  { ScrollContainer, ScrollPage } from "react-scroll-motion"
-import  { Navbar, Container, Nav, NavDropdown, Row, Col, Carousel, Card } from "react-bootstrap"
+import  { Navbar, Container, Nav, NavDropdown, Row, Col, Carousel, Card, Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { useState } from 'react';
@@ -16,7 +17,7 @@ import { useState } from 'react';
       setIndex(selectedIndex);
     };
   return (
-  <div className="App">
+  <div className="App snap">
     <header>
       <Navbar expand="lg" className="bg-body-tertiary,  center-navbar" fixed="top">
       <Container>
@@ -61,12 +62,11 @@ import { useState } from 'react';
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img
+              <img
                   className="d-block w-100"
-                  src="https://picsum.photos/900/400"
-                  alt="Second slide"
+                  src={img003}
+                  alt="First slide"
                 />
-
                 <Carousel.Caption>
                   <h3>Second slide label</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -78,18 +78,77 @@ import { useState } from 'react';
         </Container>
     </ScrollPage>
     <ScrollPage page={1}>
-        <Container fluid="true">
-          <Row>
-            <Card>
+        <Container>
+          <Row className='px-4 my-5'>
+            <Card className="text-center">
               <Card.Body>
-
-              slkjs
+                Pagina dois, testando layouts.
               </Card.Body>
             </Card>
           </Row>
+          <Row  className='px-4 my-5'>
+          <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/286/180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/id/203/286/180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/id/201/286/180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+           
+          </Row>
         </Container>
     </ScrollPage>
-      </ScrollContainer>
+    <ScrollPage page={3}>
+      <Container>
+          <Row>
+            <Col>
+              <Card>
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+    </ScrollPage>
+  </ScrollContainer>
 </div>
   );
 }
